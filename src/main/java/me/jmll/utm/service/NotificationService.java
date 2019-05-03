5af131b377bce999655d40d514909ad025029b93
alexrsolutions <salvador.alex.ramirez@gmail.com>
2019-05-02 22:19:56 -0500

    Development Finished

--- src/main/java/me/jmll/utm/service/NotificationService.java
+++
@@ -1,9 +0,0 @@
-package me.jmll.utm.service;
-
-import java.util.List;
-import me.jmll.utm.model.Notification;
-
-public interface NotificationService {
-	public List<Notification> getNotifications();
-	public Notification notify(String subject, String message, List<String> toAddress, List<String> ccAddress);
-}
