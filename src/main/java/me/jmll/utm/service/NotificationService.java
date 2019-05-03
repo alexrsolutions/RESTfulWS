9e6f174a3c7ee1312afc76b9b6e617767ca4d61d
alexrsolutions <salvador.alex.ramirez@gmail.com>
2019-05-02 20:42:44 -0500

    Notify Ready

--- src/main/java/me/jmll/utm/service/NotificationService.java
+++ src/main/java/me/jmll/utm/service/NotificationService.java
@@ -1,7 +1,9 @@
 package me.jmll.utm.service;
 
 import java.util.List;
+import me.jmll.utm.model.Notification;
 
 public interface NotificationService {
-	public void notify(String subject, String message, List<String> toAddress, List<String> ccAddress);
+	public List<Notification> getNotifications();
+	public Notification notify(String subject, String message, List<String> toAddress, List<String> ccAddress);
 }
